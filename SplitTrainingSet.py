efd066cbe9f25c8aae182f49a2f01236b9fcb2b1
EDRappaport <ElliRappaport@gmail.com>
2014-03-18 12:36:34 -0400

    starting on file to split trtainign data

---
+++ SplitTrainingSet.py
@@ -0,0 +1,43 @@
+import math
+
+# MAIN
+if len(sys.argv) != 5:
+	print("Usage: python SplitTrainingSet.py <Training Labels File> <Output Training File> <Output Test File> <percentage>")
+	sys.exit(-1)
+
+trainingLabelsFile = str(sys.argv[1])
+dir = os.path.dirname(os.path.realpath(trainingLabelsFile))
+percentage = float(sys.argv[4])
+
+fid = open(trainingLabelsFile, 'r')
+
+typeCounts = {}
+# Get numbers of each type
+for line in fid:
+	lineTok = nltk.word_tokenize(line)
+	curFile = lineTok[0]
+	curLabel = lineTok[1]
+
+	if curLabel in typeCounts:
+		typeCounts[curLabel] += 1
+	else:
+		typeCounts[curLabel] = 1
+fid.close()
+
+print typeCounts
+
+# typecounts will now contain number of items to keep in training
+for t in typeCounts:
+	typeCounts[t] = math.floor(typeCounts[t]*percentage)
+
+fid_i = open(trainingLabelsFile, 'r')
+fid_training = open(str(sys.argv[2]), 'w')
+fid_test = open(str(sys.argv[3]), 'w')
+
+for line in fid_i:
+	lineTok = nltk.word_tokenize(line)
+	curFile = lineTok[0]
+	curLabel = lineTok[1]
+
+	
+
