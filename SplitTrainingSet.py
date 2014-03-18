f3f7dcba63cdd9a29044a96412db3963f8610eb5
EDRappaport <ElliRappaport@gmail.com>
2014-03-18 12:58:42 -0400

    training split works

--- SplitTrainingSet.py
+++ SplitTrainingSet.py
@@ -1,4 +1,7 @@
 import math
+import sys
+import os
+import nltk
 
 # MAIN
 if len(sys.argv) != 5:
@@ -19,7 +22,7 @@ for line in fid:
 	curLabel = lineTok[1]
 
 	if curLabel in typeCounts:
-		typeCounts[curLabel] += 1
+		typeCounts[curLabel]+= 1
 	else:
 		typeCounts[curLabel] = 1
 fid.close()
@@ -33,11 +36,21 @@ for t in typeCounts:
 fid_i = open(trainingLabelsFile, 'r')
 fid_training = open(str(sys.argv[2]), 'w')
 fid_test = open(str(sys.argv[3]), 'w')
+fid_testAnswers = open(str(sys.argv[3])+"Answers", 'w')
 
 for line in fid_i:
 	lineTok = nltk.word_tokenize(line)
 	curFile = lineTok[0]
 	curLabel = lineTok[1]
 
-	
+	if typeCounts[curLabel] > 0:
+		fid_training.write(line)
+		typeCounts[curLabel] -= 1
+	else:
+		fid_test.write(curFile+"\n")
+		fid_testAnswers.write(line)
+
+fid_i.close()
+fid_training.close()
+fid_test.close()
 
